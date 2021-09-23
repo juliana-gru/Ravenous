98e2553f987693389d542c6cdd54bbaaddd2d434
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2021-09-23 14:04:58 -0400

    add own cors-anywhere proxy

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,7 +3,7 @@ const apiKey ='NAIFFIxD5zyMDzWePkX52tgjh_3If9quZeENNJ74s8bfb85YROvgtDdkCr7-o-1dv
 const Yelp = {
   search(term, location, sortBy) {
     return fetch(
-      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      `https://enigmatic-sierra-57012.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
         headers: {
           Authorization: `Bearer ${apiKey}`,
         }
